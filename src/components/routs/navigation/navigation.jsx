963afff39ee610891ca8832a64caa5a5f930a000
Tsion Zekaryas <tsionzekaryas49@gmail.com>
2025-10-23 12:36:02 +0300

    chore: modify folder structure and setup Firebase

---
+++ src/components/routs/navigation/navigation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../../assets/crown.svg";
+import "./navigation.scss";
+const navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="nav-links-container" to="/">
+          <CrownLogo className="logo-container " />
+        </Link>
+
+        <div>
+          <Link className="nav-links-container" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default navigation;
